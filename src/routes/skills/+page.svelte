2e1b924e1c0d908226d77b71f36f98110c5b256f
takez0_o <takez0_o@protonmail.com>
2022-12-24 01:12:40 +0300

    scrolltop

--- src/routes/skills/+page.svelte
+++ src/routes/skills/+page.svelte
@@ -1,17 +1,18 @@
 <script lang="ts">
 	import Skill from '$lib/components/skills/Skill.svelte';
 	import SkillPage from '$lib/components/skills/SkillPage.svelte';
-	import { Svroller } from 'svrollbar';
+	import { Svrollbar } from 'svrollbar';
 	import type { SkillType } from '$lib/types/Types';
-	// import VirtualList from '@sveltejs/svelte-virtual-list';
 	import fuzzy from 'fuzzy';
+	// import VirtualList from '@sveltejs/svelte-virtual-list';
 
 	export let data: any;
+	export let viewport: Element;
+	export let contents: Element;
 
 	let ghost_component: any;
 	let active_skill: SkillType;
 	let scroll_state = false;
-
 	let search_input = '';
 
 	$: feedHeight = window.innerHeight - 128;
@@ -30,7 +31,9 @@
 		} else {
 			scroll_state = false;
 		}
-		console.log(filteredSkills);
+	};
+	const scrollTop = () => {
+		viewport.scroll({ top: 0, behavior: 'smooth' });
 	};
 </script>
 
@@ -47,7 +50,7 @@
 				<div style="width:8px" />
 				<input type="text" placeholder="Search for skills" bind:value={search_input} />
 				{#if scroll_state}
-					<div class="top">
+					<div class="top link" on:click={scrollTop} on:keydown>
 						<p class="yellow">top</p>
 						<img src="icons/corner-right-up.svg" alt="go top" />
 					</div>
@@ -62,32 +65,48 @@
 				<img src="icons/chevron_passive.svg" alt="chevron" />
 			</div>
 		</div>
-		<Svroller width="520px" height={feedHeight.toString() + 'px'} on:show={updateScrollState}>
-			<div style="height:8px" />
-			<!-- <VirtualList items={data.json} let:item height={feedHeight.toString() + 'px'}>
+		<!-- <Svroller
+			width="520px"
+			height={feedHeight.toString() + 'px'}
+			on:show={updateScrollState}
+			bind:this={myel}
+		> -->
+		<div class="wrapper">
+			<div
+				bind:this={viewport}
+				class="viewport"
+				style={`width:520px; height:${feedHeight.toString() + 'px'}`}
+			>
+				<div bind:this={contents} class="contents">
+					<div style="height:8px" />
+					<!-- <VirtualList items={data.json} let:item height={feedHeight.toString() + 'px'}>
 		</VirtualList> -->
-			{#each filteredSkills as skill, index}
-				{#if index == 0}
-					<div style="height:0px" bind:this={ghost_component} />
-				{/if}
-				{#if skill.publish}
-					<div
-						on:click={() => {
-							active_skill = skill;
-						}}
-						on:keydown
-					>
-						<Skill
-							chosen={index == 1 ? true : skill == active_skill}
-							title={skill.title}
-							description={skill.description}
-							image_urls={skill.image_urls}
-							minimum_price={skill.minimum_price}
-						/>
-					</div>
-				{/if}
-			{/each}
-		</Svroller>
+					{#each filteredSkills as skill, index}
+						{#if index == 0}
+							<div style="height:0px" bind:this={ghost_component} />
+						{/if}
+						{#if skill.publish}
+							<div
+								on:click={() => {
+									active_skill = skill;
+								}}
+								on:keydown
+							>
+								<Skill
+									chosen={index == 1 ? true : skill == active_skill}
+									title={skill.title}
+									description={skill.description}
+									image_urls={skill.image_urls}
+									minimum_price={skill.minimum_price}
+								/>
+							</div>
+						{/if}
+					{/each}
+				</div>
+			</div>
+			<Svrollbar {viewport} {contents} on:show={updateScrollState} />
+		</div>
+		<!-- </Svroller> -->
 	</div>
 	<div class="skill">
 		<SkillPage skill={active_skill} />
@@ -164,5 +183,33 @@
 		display: flex;
 		flex-direction: row;
 		align-items: center;
+	}
+	.wrapper {
+		position: relative;
+		-ms-overflow-style: none; /* for Internet Explorer, Edge */
+		scrollbar-width: none; /* for Firefox */
+		overflow-y: scroll;
+		--svrollbar-track-width: 1px;
+		/* --svrollbar-track-background: #85b4b9; */
+		--svrollbar-track-opacity: 1;
+
+		--svrollbar-thumb-width: 10px;
+		--svrollbar-thumb-background: #d9ab55;
+		--svrollbar-thumb-opacity: 1;
+	}
+
+	.viewport {
+		position: relative;
+		overflow: scroll;
+		box-sizing: border-box;
+
+		/* hide scrollbar */
+		-ms-overflow-style: none;
+		scrollbar-width: none;
+	}
+
+	.viewport::-webkit-scrollbar {
+		/* hide scrollbar */
+		display: none;
 	}
 </style>
