65ab9f3b5c0f2d382b5dff645de0daf49a608d06
silvijasaule <s.aleknaite@gmail.com>
2023-01-05 11:23:04 +0200

    benefit cards skeleton + icons

---
+++ src/lib/components/landing/BenefitsCards.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+</script>
+
+<div class="container">
+    <div class="benefits-card">
+        <img src="icons/landing/lock.svg" alt="Lock icon" class="icon">
+        <h4 class="title">safe payments</h4>
+        <p class="description">A payment system that gives freelancers peace of mind knowing their time won’t be wasted, and assurance for employers.</p>
+    </div>
+    <div class="benefits-card">
+        <img src="icons/landing/mail-multiple.svg" alt="Messages icon" class="icon">
+        <h4 class="title">provable messages</h4>
+        <p class="description">Provable messages ensure fast and fair dispute resolution without compromising your personal data.</p>
+    </div>
+    <div class="benefits-card">
+        <img src="icons/landing/airdrop.svg" alt="Airdrop icon" class="icon">
+        <h4 class="title">airdrops</h4>
+        <p class="description">Keep an eye out for upcoming airdrops! All active HonestWork NFT holders will receive an airdrop with the launch of $WORK token. </p>
+    </div>
+</div>
+
+<style>
+    .container {
+        display: flex;
+        justify-content: space-between;
+        width: 100%;
+    }
+    .benefits-card {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-start;
+        padding: 20px;
+        gap: 22px;
+        background: var(--color-dark);
+        border: 1px solid rgba(255, 255, 255, 0.2);
+    }
+    .icon {
+
+    }
+    .title {
+        text-transform: uppercase;
+    }
+    .description {
+
+    }
+</style>
