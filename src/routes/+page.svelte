00bd2939e3272b9cffff98d973783b27c5a41202
silvijasaule <s.aleknaite@gmail.com>
2023-01-13 14:29:42 +0200

    faq

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,7 @@
 	import { userConnected, xmtpConnected, userState } from '$lib/stores/Network';
 	import Skeleton from '$lib/components/common/Skeleton.svelte';
 	import BenefitsCards from '$lib/components/landing/BenefitsCards.svelte';
-	import Faq from '$lib/components/landing/FAQ.svelte';
+	import Faq from '$lib/components/landing/Faq.svelte';
 
 	const track = async () => {
 		if (typeof window !== 'undefined' && window) {
