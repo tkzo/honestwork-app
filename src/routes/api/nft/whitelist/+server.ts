7af29e9f4e811e8e745fbf7b7c4dff187a469dd7
takez0_o <takez0_o@protonmail.com>
2023-03-08 22:05:58 +0300

    .

--- src/routes/api/nft/whitelist/+server.ts
+++ src/routes/api/nft/whitelist/+server.ts
@@ -1,17 +1,18 @@
 import type { RequestHandler } from '@sveltejs/kit';
 import { env } from '$env/dynamic/private';
 
-export const GET: RequestHandler = async ({ params }) => {
+export const GET = (async ({}) => {
 	const apiUrl =
 		parseInt(env.PRODUCTION_ENV) == 1
 			? env.PRIVATE_HONESTWORK_API
 			: env.PRIVATE_LOCAL_HONESTWORK_API;
-	const hwapiUrl = `${apiUrl}/favorites/${params.address}`;
+	const hwapiUrl = `${apiUrl}/whitelist`;
 	const response = await fetch(hwapiUrl, {
+		method: 'GET',
 		headers: new Headers({
 			Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
 			'Content-Type': 'application/json'
 		})
 	});
 	return response;
-};
+}) satisfies RequestHandler;
--- src/routes/mint/+page.svelte
+++ src/routes/mint/+page.svelte
@@ -9,6 +9,8 @@
 	import { nft_abi, erc20_abi } from '$lib/stores/ABI';
 	import { env } from '$env/dynamic/public';
 	import { toast } from '@zerodevx/svelte-toast';
+	import { MerkleTree } from 'merkletreejs';
+	import { keccak256 } from 'ethers/lib/utils';
 
 	type Membership = {
 		skills: number;
@@ -20,6 +22,9 @@
 	export let viewport: Element;
 	export let contents: Element;
 
+	let proof: string[];
+	let whitelist_minting = false;
+	let whitelisted: boolean;
 	let approving = false;
 	let minting: number | undefined;
 	let upgrading = false;
@@ -27,28 +32,30 @@
 	let image_url: string;
 	let next_id: number;
 	let dai_balance: number;
+	// todo: fetch from config
 	let tiers: Membership[] = [
 		{
 			skills: 3,
-			referrals: 2,
+			referrals: 1,
 			price: '$100',
 			applications: 2
 		},
 		{
 			skills: 6,
-			referrals: 3,
+			referrals: 2,
 			price: '$250',
 			applications: 3
 		},
 		{
 			skills: 8,
-			referrals: 4,
+			referrals: 3,
 			price: '$300',
 			applications: 5
 		}
 	];
 
 	$: if ($userConnected) {
+		getWhitelist();
 		fetchNextToken();
 		getUserBalance();
 	}
@@ -58,7 +65,25 @@
 			getUserBalance();
 		}, 20000);
 	});
-
+	const getWhitelist = async () => {
+		try {
+			const response = await fetch(`${base}/api/nft/whitelist`);
+			let whitelist = await response.json();
+			whitelist.find((address: string) => address == $userAddress)
+				? (whitelisted = true)
+				: (whitelisted = false);
+			console.log('Whitelisted:', whitelisted);
+			console.log('Whitelist', whitelist);
+			const leaves = whitelist.map((address: string) => keccak256(address));
+			const tree = new MerkleTree(leaves, keccak256, { sortPairs: true });
+			proof = tree.getHexProof(keccak256($userAddress));
+			console.log('Proof:', proof);
+		} catch (error) {
+			toast.push(
+				`<p class="light-60"><span style='color:var(--color-error)'>error: </span>${error}</p>`
+			);
+		}
+	};
 	const fetchNextToken = async () => {
 		refreshing = true;
 		try {
@@ -73,7 +98,6 @@
 		}
 		refreshing = false;
 	};
-
 	const getUserBalance = async () => {
 		if ($userConnected) {
 			try {
@@ -87,7 +111,6 @@
 			}
 		}
 	};
-
 	const approve = async (amount: ethers.BigNumberish) => {
 		approving = true;
 		const Token = new ethers.Contract(env.PUBLIC_MAINNET_DAI_ADDRESS, erc20_abi, $networkSigner);
@@ -104,7 +127,6 @@
 		}
 		approving = false;
 	};
-
 	const mint = async (index: number) => {
 		minting = index;
 		if (index > 0) {
@@ -142,7 +164,6 @@
 		}
 		minting = undefined;
 	};
-
 	const upgrade = async (index: number) => {
 		upgrading = true;
 		// todo: remove hardcoded values
@@ -172,6 +193,22 @@
 		}
 		upgrading = false;
 	};
+	const whitelistMint = async () => {
+		try {
+			const contract = new ethers.Contract(env.PUBLIC_NFT_ADDRESS, nft_abi, $networkSigner);
+			const tx = await contract.whitelistMint(proof);
+			const receipt = await tx.wait();
+			if (receipt.status == 1) {
+				toast.push(
+					`<p class="light-60"><span style='color:var(--color-success)'>success: </span>Minted via whitelist</p>`
+				);
+			}
+		} catch (error: any) {
+			toast.push(
+				`<p class="light-60"><span style='color:var(--color-error)'>error: </span>${error.reason}</p>`
+			);
+		}
+	};
 </script>
 
 <svelte:head>
@@ -179,81 +216,141 @@
 	<meta name="description" content="HonestWork Genesis Minting" />
 </svelte:head>
 
+<div style="height: 16px" />
 {#if $userConnected}
 	<div class="wrapper">
 		<div bind:this={viewport} class="viewport">
 			<div bind:this={contents} class="contents">
-				<main>
-					{#each tiers as tier, i}
-						<section class="border-all">
+				{#if !whitelisted}
+					<main>
+						{#each tiers as tier, i}
+							<section class="border-all">
+								<div class="mint-header">
+									<p class="yellow">
+										genesis tier
+										{#each new Array(i + 1) as _}
+											i
+										{/each}
+									</p>
+								</div>
+								<div style="height:8px" />
+								<div class="mint-feature">
+									<p class="light-60">skills</p>
+									<p>{tier.skills}</p>
+								</div>
+								<div style="height:8px" />
+								<div class="mint-feature">
+									<p class="light-60">referrals</p>
+									<p>{tier.referrals}</p>
+								</div>
+								<div style="height:8px" />
+								<div class="mint-feature">
+									<p class="light-60">price</p>
+									<p>{tier.price}</p>
+								</div>
+								<div style="height:8px" />
+								<div class="mint-feature">
+									<p class="light-60">applications/day</p>
+									<p>{tier.applications}</p>
+								</div>
+								<div style="height:28px" />
+								<div class="divider" />
+								<div style="height:28px" />
+								<div class="mint-feature">
+									<p class="light-60">payment token</p>
+									<p>dai</p>
+								</div>
+								<div style="height:8px" />
+								<div class="mint-feature">
+									<p class="light-60">your balance</p>
+									<p>
+										{dai_balance?.toLocaleString('en-US', {
+											style: 'currency',
+											currency: 'USD'
+										})}
+									</p>
+								</div>
+								<div style="height:28px" />
+								<div class="button link" on:click={() => mint(i)} on:keydown>
+									{#if minting == i}
+										<img src={`${assets}/icons/loader.svg`} alt="loading" class="rotating" />
+										<div style="width:4px" />
+									{/if}
+									{#if i == 0}
+										{#if approving}
+											<p class="yellow">approving</p>
+										{:else if minting}
+											<p class="yellow">minting</p>
+										{:else if upgrading}
+											<p class="yellow">upgrading</p>
+										{:else}
+											<p class={`${$userState == 0 ? 'yellow' : 'light-40'}`}>mint</p>
+										{/if}
+									{:else}
+										<p class={`${$userState > 0 ? 'yellow' : 'light-40'}`}>mint & upgrade</p>
+									{/if}
+								</div>
+							</section>
+						{/each}
+					</main>
+				{:else}
+					<main>
+						<div>
+							<div class="header">
+								<img
+									src={`${assets}/icons/human-handsup.svg`}
+									alt="whitelisted"
+									class="small-icon"
+								/>
+								<div style="width:4px" />
+								<p>Congratulations!</p>
+							</div>
+							<div style="height:16px" />
+							<p class="light-80" style="width:360px">
+								You get all the benefits of a <span class="yellow">tier I membership</span> worth $100.
+							</p>
+							<br />
+							<p class="light-80">Happy to have you onboard 🥰</p>
+						</div>
+						<section class="border-all" style="width:480px;">
 							<div class="mint-header">
-								<p class="yellow">
-									genesis tier
-									{#each new Array(i + 1) as _}
-										i
-									{/each}
-								</p>
+								<p class="yellow">whitelist tier i</p>
 							</div>
 							<div style="height:8px" />
 							<div class="mint-feature">
 								<p class="light-60">skills</p>
-								<p>{tier.skills}</p>
+								<p>{tiers[0].skills}</p>
 							</div>
 							<div style="height:8px" />
 							<div class="mint-feature">
 								<p class="light-60">referrals</p>
-								<p>{tier.referrals}</p>
+								<p>{tiers[0].referrals}</p>
 							</div>
 							<div style="height:8px" />
 							<div class="mint-feature">
 								<p class="light-60">price</p>
-								<p>{tier.price}</p>
+								<p>{tiers[0].price}</p>
 							</div>
 							<div style="height:8px" />
 							<div class="mint-feature">
 								<p class="light-60">applications/day</p>
-								<p>{tier.applications}</p>
+								<p>{tiers[0].applications}</p>
 							</div>
 							<div style="height:28px" />
-							<div class="divider" />
-							<div style="height:28px" />
-							<div class="mint-feature">
-								<p class="light-60">payment token</p>
-								<p>dai</p>
-							</div>
-							<div style="height:8px" />
-							<div class="mint-feature">
-								<p class="light-60">your balance</p>
-								<p>
-									{dai_balance?.toLocaleString('en-US', {
-										style: 'currency',
-										currency: 'USD'
-									})}
-								</p>
-							</div>
-							<div style="height:28px" />
-							<div class="button link" on:click={() => mint(i)} on:keydown>
-								{#if minting == i}
+
+							<div class="button link" on:click={whitelistMint} on:keydown>
+								{#if whitelist_minting}
 									<img src={`${assets}/icons/loader.svg`} alt="loading" class="rotating" />
 									<div style="width:4px" />
-								{/if}
-								{#if i == 0}
-									{#if approving}
-										<p class="yellow">approving</p>
-									{:else if minting}
-										<p class="yellow">minting</p>
-									{:else if upgrading}
-										<p class="yellow">upgrading</p>
-									{:else}
-										<p class={`${$userState == 0 ? 'yellow' : 'light-40'}`}>mint</p>
-									{/if}
+									<p class="yellow">minting</p>
 								{:else}
-									<p class={`${$userState > 0 ? 'yellow' : 'light-40'}`}>mint & upgrade</p>
+									<p class="yellow">free mint (only gas)</p>
 								{/if}
 							</div>
 						</section>
-					{/each}
-				</main>
+					</main>
+				{/if}
+
 				<div style="height:12px" />
 				<div class="buy-tokens">
 					<a href="https://app.uniswap.org/#/swap" target="_blank" rel="noreferrer"
@@ -268,7 +365,7 @@
 					<p class="yellow">next mint</p>
 					<p>genesis #{next_id}</p>
 				</div>
-				<img src={refreshing ? placeholder_image : image_url} alt="Next NFT" />
+				<img src={refreshing ? placeholder_image : image_url} alt="Next NFT" class="mint-image" />
 				<div style="height: 120px" />
 			</div>
 		</div>
@@ -318,7 +415,7 @@
 		justify-content: space-between;
 		align-items: center;
 	}
-	img {
+	.mint-image {
 		width: 760px;
 		height: 760px;
 	}
@@ -386,6 +483,12 @@
 		display: flex;
 		flex-direction: row;
 		justify-content: center;
+		align-items: center;
+	}
+	.header {
+		display: flex;
+		flex-direction: row;
+		justify-content: flex-start;
 		align-items: center;
 	}
 </style>
