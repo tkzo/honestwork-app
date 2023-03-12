874463081dfa3561d2feefe2867a7b7d2996231b
takez0_o <takez0_o@protonmail.com>
2023-03-12 19:40:39 +0300

    .

--- tests/test.ts
+++ tests/test.ts
@@ -1,5 +1,6 @@
 import { expect, test } from '@playwright/test';
 
+// test
 test('about page has expected h1', async ({ page }) => {
 	await page.goto('/jobs');
 	expect(await page.textContent('h1')).toBe('About this app');
