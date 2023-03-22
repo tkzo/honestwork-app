bec8123f8373c38c3231a0540a7e1aaa2751bb2f
takez0_o <takez0_o@protonmail.com>
2023-03-22 15:52:39 +0300

    .

--- src/routes/profile/+layout.server.ts
+++ src/routes/profile/+layout.server.ts
@@ -3,80 +3,80 @@ import type { LayoutServerLoad } from './$types';
 import { env } from '$env/dynamic/private';
 
 const apiUrl =
-	parseInt(env.PRODUCTION_ENV) == 1 ? env.PRIVATE_HONESTWORK_API : env.PRIVATE_LOCAL_HONESTWORK_API;
+  parseInt(env.PRODUCTION_ENV) == 1 ? env.PRIVATE_HONESTWORK_API : env.PRIVATE_LOCAL_HONESTWORK_API;
 
 export const load: LayoutServerLoad = async ({ cookies }) => {
-	const userAddress = cookies.get('honestwork_address');
-	const userSignature = cookies.get('honestwork_signature');
+  const userAddress = cookies.get('honestwork_address');
+  const userSignature = cookies.get('honestwork_signature');
 
-	if (userAddress && userSignature) {
-		const callUrl = `${apiUrl}/verify/${userAddress}/${userSignature}`;
-		let callResponse = await fetch(callUrl, {
-			headers: new Headers({
-				Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
-				'Content-Type': 'application/json'
-			})
-		});
-		let calldata = await callResponse.json();
-		if (calldata == 'success') {
-			let user = await getUser(userAddress);
-			let skills = await getSkills(userAddress);
-			let jobs = await getJobs(userAddress);
-			user.address = userAddress;
-			return { user: user, skills: skills, jobs: jobs };
-		} else {
-			throw redirect(301, '/auth');
-		}
-	}
+  if (userAddress && userSignature) {
+    const callUrl = `${apiUrl}/verify/${userAddress}/${userSignature}`;
+    let callResponse = await fetch(callUrl, {
+      headers: new Headers({
+        Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
+        'Content-Type': 'application/json'
+      })
+    });
+    let calldata = await callResponse.json();
+    if (calldata == 'success') {
+      let user = await getUser(userAddress);
+      let skills = await getSkills(userAddress, userSignature);
+      let jobs = await getJobs(userAddress);
+      user.address = userAddress;
+      return { user: user, skills: skills, jobs: jobs };
+    } else {
+      throw redirect(301, '/auth');
+    }
+  }
 };
 
 const getUser = async (address: string) => {
-	const url = `${apiUrl}/users/${address}`;
-	let response = await fetch(url, {
-		headers: new Headers({
-			Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
-			'Content-Type': 'application/json'
-		})
-	});
-	if (response.ok) {
-		let json = await response.json();
-		return json;
-	} else {
-		console.log('HTTP-Error: ' + response.status);
-		return response.status;
-	}
+  const url = `${apiUrl}/users/${address}`;
+  let response = await fetch(url, {
+    headers: new Headers({
+      Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
+      'Content-Type': 'application/json'
+    })
+  });
+  if (response.ok) {
+    let json = await response.json();
+    return json;
+  } else {
+    console.log('HTTP-Error: ' + response.status);
+    return response.status;
+  }
 };
 
-const getSkills = async (address: string) => {
-	const url = `${apiUrl}/skills/${address}`;
-	let response = await fetch(url, {
-		headers: new Headers({
-			Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
-			'Content-Type': 'application/json'
-		})
-	});
-	if (response.ok) {
-		let json = await response.json();
-		return { json: json };
-	} else {
-		console.log('HTTP-Error: ' + response.status);
-		return response.status;
-	}
+const getSkills = async (address: string, signature: string) => {
+  const url = `${apiUrl}/skills/${address}/${signature}`;
+  let response = await fetch(url, {
+    headers: new Headers({
+      Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
+      'Content-Type': 'application/json'
+    })
+  });
+  if (response.ok) {
+    let json = await response.json();
+    return { json: json };
+  } else {
+    console.log('HTTP-Error: ' + response.status);
+    return response.status;
+  }
 };
 
 const getJobs = async (address: string) => {
-	const url = `${apiUrl}/jobs/${address}`;
-	let response = await fetch(url, {
-		headers: new Headers({
-			Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
-			'Content-Type': 'application/json'
-		})
-	});
-	if (response.ok) {
-		let json = await response.json();
-		return { json };
-	} else {
-		console.log('HTTP-Error: ' + response.status);
-		return response.status;
-	}
+  const url = `${apiUrl}/jobs/${address}`;
+  let response = await fetch(url, {
+    headers: new Headers({
+      Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
+      'Content-Type': 'application/json'
+    })
+  });
+  if (response.ok) {
+    let json = await response.json();
+    return { json };
+  } else {
+    console.log('HTTP-Error: ' + response.status);
+    return response.status;
+  }
 };
--- src/routes/profile/skills/+page.server.ts
+++ src/routes/profile/skills/+page.server.ts
@@ -3,43 +3,43 @@ import type { PageServerLoad } from './$types';
 import { env } from '$env/dynamic/private';
 
 const apiUrl =
-	parseInt(env.PRODUCTION_ENV) == 1 ? env.PRIVATE_HONESTWORK_API : env.PRIVATE_LOCAL_HONESTWORK_API;
+  parseInt(env.PRODUCTION_ENV) == 1 ? env.PRIVATE_HONESTWORK_API : env.PRIVATE_LOCAL_HONESTWORK_API;
 
 export const load: PageServerLoad = async ({ cookies }) => {
-	const userAddress = cookies.get('honestwork_address');
-	const userSignature = cookies.get('honestwork_signature');
+  const userAddress = cookies.get('honestwork_address');
+  const userSignature = cookies.get('honestwork_signature');
 
-	if (userAddress && userSignature) {
-		const callUrl = `${apiUrl}/verify/${userAddress}/${userSignature}`;
-		let callResponse = await fetch(callUrl, {
-			headers: new Headers({
-				Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
-				'Content-Type': 'application/json'
-			})
-		});
-		let calldata = await callResponse.json();
-		if (calldata == 'success') {
-			let skills = await getSkills(userAddress);
-			return { skills: skills };
-		} else {
-			throw redirect(301, '/auth');
-		}
-	}
+  if (userAddress && userSignature) {
+    const callUrl = `${apiUrl}/verify/${userAddress}/${userSignature}`;
+    let callResponse = await fetch(callUrl, {
+      headers: new Headers({
+        Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
+        'Content-Type': 'application/json'
+      })
+    });
+    let calldata = await callResponse.json();
+    if (calldata == 'success') {
+      let skills = await getSkills(userAddress, userSignature);
+      return { skills: skills };
+    } else {
+      throw redirect(301, '/auth');
+    }
+  }
 };
 
-const getSkills = async (address: string) => {
-	const url = `${apiUrl}/skills/${address}`;
-	let response = await fetch(url, {
-		headers: new Headers({
-			Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
-			'Content-Type': 'application/json'
-		})
-	});
-	if (response.ok) {
-		let json = await response.json();
-		return { json: json };
-	} else {
-		console.log('HTTP-Error: ' + response.status);
-		return response.status;
-	}
+const getSkills = async (address: string, signature: string) => {
+  const url = `${apiUrl}/skills_member/${address}/${signature}`;
+  let response = await fetch(url, {
+    headers: new Headers({
+      Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
+      'Content-Type': 'application/json'
+    })
+  });
+  if (response.ok) {
+    let json = await response.json();
+    return { json: json };
+  } else {
+    console.log('HTTP-Error: ' + response.status);
+    return response.status;
+  }
 };
