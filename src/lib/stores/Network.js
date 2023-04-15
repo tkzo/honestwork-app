057e783f2eca4637704c3fa9588e447a095c5ac0
takez0_o <takez0_o@protonmail.com>
2023-04-15 22:38:25 +0300

    .

--- src/lib/stores/Network.js
+++ src/lib/stores/Network.js
@@ -22,89 +22,89 @@ export let xmtpConnected = writable(false);
 export let xmtpConnecting = writable(false);
 
 let chain_names = {
-	1: 'mainnet',
-	42161: 'arbitrum'
+  1: 'mainnet',
+  42161: 'arbitrum'
 };
 
 export const connectIfCached = async () => {
-	if (!get(userConnected)) {
-		let local = localStorage.getItem('honestwork_connection', 'true');
-		if (local && local == 'true') {
-			connectWallet();
-		}
-	}
+  if (!get(userConnected)) {
+    let local = localStorage.getItem('honestwork_connection', 'true');
+    if (local && local == 'true') {
+      connectWallet();
+    }
+  }
 };
 
 export const connectWallet = async () => {
-	connecting.set(true);
-	try {
-		const provider = new ethers.providers.Web3Provider(window.ethereum);
-		await provider.send('eth_requestAccounts', []);
-		const signer = provider.getSigner();
-		networkProvider.set(provider);
-		networkSigner.set(signer);
+  connecting.set(true);
+  try {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    await provider.send('eth_requestAccounts', []);
+    const signer = provider.getSigner();
+    networkProvider.set(provider);
+    networkSigner.set(signer);
 
-		let addr = await signer.getAddress();
-		userAddress.set(addr);
-		let chain_id = await signer.getChainId();
-		chainID.set(chain_id);
-		for (const [key, value] of Object.entries(chain_names)) {
-			if (key == chain_id) {
-				chainName.set(value);
-			}
-		}
-		userConnected.set(true);
-		const url = `${base}/api/nft/user-token-id/${addr}`;
-		const res = await fetch(url);
-		const token = await res.json();
-		userToken.set(token.token_id);
-		toast.push(
-			`<p class="light-60"><span style='color:var(--color-success)'>success: </span>wallet connected.</p>`
-		);
-		let local = localStorage.getItem('honestwork_connection', 'true');
-		if (local != 'true') {
-			localStorage.setItem('honestwork_connection', 'true');
-		}
-		setListeners();
-		connecting.set(false);
-		await fetchUserState();
-	} catch (err) {
-		console.log('error:', err);
-	}
+    let addr = await signer.getAddress();
+    userAddress.set(addr);
+    let chain_id = await signer.getChainId();
+    chainID.set(chain_id);
+    for (const [key, value] of Object.entries(chain_names)) {
+      if (key == chain_id) {
+        chainName.set(value);
+      }
+    }
+    userConnected.set(true);
+    const url = `${base}/api/nft/user-token-id/${addr}`;
+    const res = await fetch(url);
+    const token = await res.json();
+    userToken.set(token.token_id);
+    toast.push(
+      `<p class="light-60"><span style='color:var(--color-success)'>success: </span>wallet connected.</p>`
+    );
+    let local = localStorage.getItem('honestwork_connection', 'true');
+    if (local != 'true') {
+      localStorage.setItem('honestwork_connection', 'true');
+    }
+    setListeners();
+    connecting.set(false);
+    await fetchUserState();
+  } catch (err) {
+    console.log('error:', err);
+  }
 };
 
 export const connectXmtp = async () => {
-	try {
-		if (!get(xmtpConnected)) {
-			xmtpConnecting.set(true);
-			let xc = await Client.create(get(networkSigner));
-			xmtpClient.set(xc);
-			xmtpConnecting.set(false);
-			xmtpConnected.set(true);
-			toast.push(
-				`<p class="light-60"><span style='color:var(--color-success)'>success: </span>xmtp connected.</p>`
-			);
-		}
-	} catch (err) {
-		console.log('error:', err);
-	}
+  try {
+    if (!get(xmtpConnected)) {
+      xmtpConnecting.set(true);
+      let xc = await Client.create(get(networkSigner));
+      xmtpClient.set(xc);
+      xmtpConnecting.set(false);
+      xmtpConnected.set(true);
+      toast.push(
+        `<p class="light-60"><span style='color:var(--color-success)'>success: </span>xmtp connected.</p>`
+      );
+    }
+  } catch (err) {
+    console.log('error:', err);
+  }
 };
 
 const setListeners = () => {
-	window.ethereum.on('accountsChanged', function () {
-		window.location.reload();
-	});
-	window.ethereum.on('chainChanged', function () {
-		window.location.reload();
-	});
+  window.ethereum.on('accountsChanged', function() {
+    window.location.reload();
+  });
+  window.ethereum.on('chainChanged', function() {
+    window.location.reload();
+  });
 };
 
 const fetchUserState = async () => {
-	try {
-		let res = await fetch(`${base}/api/membership/${get(userAddress)}`);
-		let tier = await res.json();
-		userState.set(tier);
-	} catch (err) {
-		console.log(err);
-	}
+  try {
+    let res = await fetch(`${base}/api/membership/${get(userAddress)}`);
+    let tier = await res.json();
+    userState.set(tier);
+  } catch (err) {
+    console.log(err);
+  }
 };
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,41 +1,42 @@
-import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 import { env } from '$env/dynamic/private';
 import { base } from '$app/paths';
 import type { APIConfig } from '$lib/stores/Types';
 
 const apiUrl =
-	parseInt(env.PRODUCTION_ENV) == 1 ? env.PRIVATE_HONESTWORK_API : env.PRIVATE_LOCAL_HONESTWORK_API;
+  parseInt(env.PRODUCTION_ENV) == 1 ? env.PRIVATE_HONESTWORK_API : env.PRIVATE_LOCAL_HONESTWORK_API;
 
 export const load = (async ({ cookies, fetch }) => {
-	// todo: implement config
-	const config_url = `${base}/api/config`;
-	let config_response = await fetch(config_url, {
-		method: 'GET',
-		headers: new Headers({
-			Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
-			'Content-Type': 'application/json'
-		})
-	});
-	let config_data: APIConfig = await config_response.json();
+  // todo: implement config
+  const config_url = `${base}/api/config`;
+  let config_response = await fetch(config_url, {
+    method: 'GET',
+    headers: new Headers({
+      Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
+      'Content-Type': 'application/json'
+    })
+  });
+  let config_data: APIConfig = await config_response.json();
 
-	const userAddress = cookies.get('honestwork_address');
-	const userSignature = cookies.get('honestwork_signature');
+  const userAddress = cookies.get('honestwork_address');
+  const userSignature = cookies.get('honestwork_signature');
 
-	if (userAddress != null && userSignature != null) {
-		const callUrl = `${apiUrl}/verify/${userAddress}/${userSignature}`;
-		let callResponse = await fetch(callUrl, {
-			headers: new Headers({
-				Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
-				'Content-Type': 'application/json'
-			})
-		});
-		let calldata = await callResponse.json();
-		if (calldata == 'success') {
-			return {
-				signed: true
-			};
-		} else {
-		}
-	}
+  if (userAddress != null && userSignature != null) {
+    const callUrl = `${apiUrl}/verify/${userAddress}/${userSignature}`;
+    let callResponse = await fetch(callUrl, {
+      headers: new Headers({
+        Authorization: 'Basic ' + btoa(`${env.PRIVATE_CLIENT_KEY}:${env.PRIVATE_CLIENT_PASSWORD}`),
+        'Content-Type': 'application/json'
+      })
+    });
+    console.log("CallResponse:", callResponse);
+    let calldata = await callResponse.json();
+    console.log("Calldata:", calldata);
+    if (calldata == 'success') {
+      return {
+        signed: true
+      };
+    } else {
+    }
+  }
 }) satisfies LayoutServerLoad;
